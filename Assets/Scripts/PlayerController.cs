4a134335927e84c05847c79552a91de2c7c4202a
ihatepointers <51532583+ihatepointers@users.noreply.github.com>
2021-12-04 22:44:39 +0300

    respawn

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,44 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 5f;
-    public float JumpHeight = 2f;
-    public float GroundDistance = 0.2f;
-    public float DashDistance = 5f;
-    public LayerMask Ground;
-
-    private Rigidbody _body;
-    private Vector3 _inputs = Vector3.zero;
-    private bool _isGrounded = true;
-    private Transform _groundChecker;
-
+    private Rigidbody _rb;
+    private CapsuleCollider _collider;
+    public Transform _spawnPoint;
+    private GameManager _gameManager;
     void Start()
     {
-        _body = GetComponent<Rigidbody>();
-        _groundChecker = transform.GetChild(0);
+        _rb = GetComponent<Rigidbody>();
+        _collider = GetComponent<CapsuleCollider>();
+        _gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     void Update()
     {
-        _isGrounded = Physics.CheckSphere(_groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
 
-
-        _inputs = Vector3.zero;
-        _inputs.x = Input.GetAxis("Horizontal");
-        _inputs.z = Input.GetAxis("Vertical");
-        if (_inputs != Vector3.zero)
-            transform.forward = _inputs;
-
-        if (Input.GetButtonDown("Jump") && _isGrounded)
-        {
-            _body.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
-        }
 
     }
 
-
-    void FixedUpdate()
+    private void OnTriggerEnter(Collider other)
     {
-        _body.MovePosition(_body.position + _inputs * Speed * Time.fixedDeltaTime);
+        Debug.Log(other.transform.name);
+        if (other.transform.CompareTag("Red"))
+        {
+            this.transform.position = _spawnPoint.position;
+        }
     }
+
 }
