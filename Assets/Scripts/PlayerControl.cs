6c53416ab7dd6418b122f987feb3aee0bd1c797f
ihatepointers <51532583+ihatepointers@users.noreply.github.com>
2021-12-04 15:32:25 +0300

    merge-

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public CharacterController controller;
+    private bool isGrounded;
+    public LayerMask groundMask;
+    public Transform groundCheck;
+
+    private float groundDistance = 0.4f;
+
+    Vector3 velocity;
     
     [Header("Values")]
     public float speed = 10f;
+    public float gravity = -10;
+    public float jumpHeight = 2f;
+    
     void Start()
     {
         
@@ -16,6 +26,22 @@ public class PlayerControl : MonoBehaviour
     
     void Update()
     {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        controller.Move(velocity * Time.deltaTime);
+        
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
